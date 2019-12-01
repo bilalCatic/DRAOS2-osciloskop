14b21c8f3fec5a27a24f9c8c8d3dddf87b14e13d
Bilal Catic <bilal.catic@hotmail.com>
2019-12-01 05:45:40 +0100

    send only visible data to the Screen component

--- src/components/Oscilloscope/components/Screen/index.js
+++ src/components/Oscilloscope/components/Screen/index.js
@@ -1,8 +1,93 @@
-import { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
+import ApexCharts from 'apexcharts';
+import ReactApexChart from 'react-apexcharts';
 
 class Screen extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: {
+                tooltip: {
+                    enabled: false
+                },
+                grid: {
+                    show: true,
+                    xaxis: {
+                        lines: {
+                            show: true
+                        }
+                    },
+                    yaxis: {
+                        lines: {
+                            show: true
+                        }
+                    }
+                },
+                chart: {
+                    id: 'realtime',
+                    animations: {
+                        enabled: true,
+                        easing: 'linear',
+                        dynamicAnimation: {
+                            speed: 1000
+                        }
+                    },
+                    toolbar: {
+                        show: false
+                    },
+                    zoom: {
+                        enabled: false
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    curve: 'smooth'
+                },
+                markers: {
+                    size: 0
+                },
+                xaxis: {
+                    type: 'numeric',
+                    labels: {
+                        show: false
+                    },
+                    tickAmount: 18,
+                },
+                yaxis: {
+                    type: 'numeric',
+                    min: -1,
+                    max: 1,
+                    labels: {
+                        show: false
+                    },
+                    tickAmount: 8,
+                },
+                legend: {
+                    show: false
+                }
+            },
+            startIndex: 0,
+            intervalHandle: null,
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const { channel1Data: data1, channel2Data: data2 } = this.props;
+
+        if (Array.isArray(data1) && Array.isArray(data2)){
+            ApexCharts.exec('realtime', 'updateSeries', [{data: data1}, {data: data2}]);
+        }
+    }
+
     render() {
-        return null;
+        return (
+            <div id="chart">
+                <ReactApexChart options={this.state.options} series={[]} type="line" height="350" />
+            </div>
+        );
     }
 }
 
--- src/components/Oscilloscope/index.js
+++ src/components/Oscilloscope/index.js
@@ -1,16 +1,87 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Grid } from 'semantic-ui-react';
 
 import Screen from "./components/Screen";
 
-const Oscilloscope = () => {
-    return (
-        <Grid.Row>
-            <Grid.Column>
-                <Screen />
-            </Grid.Column>
-        </Grid.Row>
-    );
-};
+const W = 2*Math.PI;
+const DELTA_T = 0.01; // 10ms
+
+const REFRESH_INTERVAL = 100;
+const WINDOW_SIZE = 100;
+
+class Oscilloscope extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            channel1Data: new Array(WINDOW_SIZE).fill(0),
+            channel2Data: new Array(WINDOW_SIZE).fill(0),
+            channel1: {
+                inputDataGenerator: this.functionNextValue(Math.sin, DELTA_T, W),
+            },
+            channel2: {
+                inputDataGenerator: this.functionNextValue(Math.cos, DELTA_T, W),
+            },
+            intervalHandler: null,
+        }
+    }
+
+    componentDidMount() {
+        const intervalHandler = window.setInterval(this.shiftChannelValues.bind(this), REFRESH_INTERVAL);
+        this.setState({intervalHandler});
+    }
+
+    componentWillUnmount() {
+        const { intervalHandler } = this.state;
+        window.clearInterval(intervalHandler);
+    }
+
+    render() {
+        const { channel1Data, channel2Data } = this.state;
+
+        return (
+            <Grid.Row>
+                <Screen
+                    channel1Data={channel1Data}
+                    channel2Data={channel2Data}
+                />
+            </Grid.Row>
+        );
+    }
+
+    //****************************//
+
+    * functionNextValue (mathFunction, timeDelta = DELTA_T, argument = 1, startTime = 0) {
+        if (!(mathFunction && mathFunction.constructor && mathFunction.call && mathFunction.apply)) {
+            return;
+        }
+
+        let time = startTime;
+        while (true){
+            yield mathFunction(argument*time);
+            time += timeDelta;
+        }
+    }
+
+    shiftChannelValues() {
+        const { channel1Data, channel2Data, channel1, channel2 } = this.state;
+        const inputDataGenerator1 = channel1.inputDataGenerator;
+        const inputDataGenerator2 = channel2.inputDataGenerator;
+
+        const nextData1 = inputDataGenerator1.next().value;
+        const nextData2 = inputDataGenerator2.next().value;
+
+        let newChannel1Data = [];
+        let newChannel2Data = [];
+        if (nextData1 !== undefined){
+            newChannel1Data = [...channel1Data.slice(1), nextData1];
+        }
+        if (nextData2 !== undefined){
+            newChannel2Data = [...channel2Data.slice(1), nextData2];
+        }
+
+        this.setState({channel1Data: newChannel1Data, channel2Data: newChannel2Data});
+    }
+}
 
 export default Oscilloscope;
